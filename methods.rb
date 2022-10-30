5c9b6d68ed6c903734d31b461006a8c0618bd38f
Chris Li <chris.lty07@gmail.com>
2022-10-30 10:34:20 -0400

    commit tree 32c1729b66bce7d825172f0c997a34adfc41b07c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+#First Function
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+#Second Function
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+#Third Function
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+end
+
+#Fourth Function
+def add(num1, num2) 
+    num1 + num2 
+end
+
+#Fifth Function
+def halve number 
+    if number.class != Integer
+        return nil
+    end 
+
+    number / 2
+end 
+
